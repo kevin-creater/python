f8bc6115b787c6477f2fc643190f2c3a93823af6
kevin-creater <kevinjoseph2028@cs.sjcetpalai.ac.in>
2024-10-01 14:58:27 +0530

    first commit

---
+++ student details.py
@@ -0,0 +1,7 @@
+name=input("enter your name:")
+roll_number=int(input("enter your roll number:"))
+cgpa=float(input("enter your cgpa:"))
+percentage =cgpa*10
+print("name of the student:",name)
+print("roll number:",roll_number)
+print("your percentage is:",percentage,"%")
