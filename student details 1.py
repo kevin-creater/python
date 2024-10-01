d046c686fb7db9474d9df1a6eef52b83cbae9569
kevin-creater <kevinjoseph2028@cs.sjcetpalai.ac.in>
2024-10-01 15:23:18 +0530

    document head add

---
+++ student details 1.py
@@ -0,0 +1,13 @@
+'''
+python program to get the students
+Author:kevin joseph
+date:01-10-2024
+version:1.0
+'''
+name=input("enter your name:")
+roll_number=int(input("enter your roll number:"))
+cgpa=float(input("enter your cgpa:"))
+percentage =cgpa*10
+print("name of the student:",name)
+print("roll number:",roll_number)
+print("your percentage is:",percentage,"%")
