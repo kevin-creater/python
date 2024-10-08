caeae6f94c30f015a86d06451065d30e2d023173
kevin-creater <kevinjoseph2028@cs.sjcetpalai.ac.in>
2024-10-08 14:58:42 +0530

    Add files via upload

---
+++ kevin task 2.py
@@ -0,0 +1,13 @@
+#author:kevin joseph
+#date:8/10/2024
+
+first_name="kevin"
+last_name="joseph"
+full_name= first_name +" "+last_name
+print(full_name)
+length = len(first_name)
+print(length)
+extracted_last_name =full_name[length+1:]
+print(full_name)
+print(extracted_last_name)
+
