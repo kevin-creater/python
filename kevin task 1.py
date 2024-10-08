57d9384a14b11ef15c12fed05d1e75071cf64a4d
kevin-creater <kevinjoseph2028@cs.sjcetpalai.ac.in>
2024-10-08 14:38:02 +0530

    Add files via upload

---
+++ kevin task 1.py
@@ -0,0 +1,21 @@
+num_1=int(input("enter the first number:"))
+num_2=int(input("enter the second number:"))
+num_3=int(input("enter the third number:"))
+#addition of first two numbers
+add=num_1+num_2
+print("The sum of num1 and num2 is",add)
+#subtration
+sub=num_2-num_1
+print("The difference when num2 is subtracted from num1",sub)
+#multiplication
+mult=num_1*num_2
+print( "The product of num1 and num2 is:",mult)
+#division
+div=num_1/num_2
+print("the quotient when num1 is divided by num2 is:",div)
+#modulus
+mod=num_1%num_2
+print("The remainder when num1 is divided by num2 is:",mod)
+#arithmetic operation
+arth=(num_1 + num_2) * num_3 / 2
+print("The result of (num_1 + num_2) * num_3 / 2 is:",arth)
