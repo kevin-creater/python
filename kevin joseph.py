542533b43457e7db3f826178578336fc394e8564
kevin-creater <kevinjoseph2028@cs.sjcetpalai.ac.in>
2024-10-15 14:51:06 +0530

    Add files via upload

--- kevin joseph.py
+++ kevin joseph.py
@@ -1,9 +1,17 @@
 '''
-python program to check largest of two numbers
+python program to determine the smallest of three numbers
 '''
-principle=int(input("enter the principle value :"))
-years=int(input("enter the number of years:"))
-rate=float(input("enter the rate:"))
-simple_interest=principle*years*rate
-print(simple_interest)
+num_1=int(input("enter the first number:"))
+num_2=int(input("enter the second number:"))
+num_3=int(input("enter the third number:"))
+if num_1<num_2 and num_3:
+     print("smallest:",num_1)
+elif num_2<num_1 and num_2<num_3:
+     print ("smallest:",num_2)
+else:
+     print(num_3)
+
+
+
+
 
