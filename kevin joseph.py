aace4c57b7ce77079d9c954b3b01c46c4f367684
kevin-creater <kevinjoseph2028@cs.sjcetpalai.ac.in>
2024-10-15 16:31:46 +0530

    Add files via upload

--- kevin joseph.py
+++ kevin joseph.py
@@ -2,9 +2,13 @@
 python program to find sum of digits of number
 author:kevin joseph
 '''
-
-
-
+num=int(input("enter a number:"))
+sum=0
+while(num>0):
+     r=num%10
+     num=num//10
+     sum=sum+r
+print(sum)
 
 
 
