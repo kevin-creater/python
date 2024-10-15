b5727fff4a616de9a6443dd80a1bdd0ffa4263e7
kevin-creater <kevinjoseph2028@cs.sjcetpalai.ac.in>
2024-10-15 14:03:28 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,7 @@
+first_name=input("enter your first name:")
+last_name=input("enter your last name:")
+full_name=first_name+" "+last_name
+print(full_name)
+length=len(first_name)
+extract_first_name=full_name[:length]
+print(extract_first_name)
