645268db9953d0ecf34615fc12750ccdb81d992a
kevin-creater <kevinjoseph2028@cs.sjcetpalai.ac.in>
2024-10-22 15:02:04 +0530

    Add files via upload

--- kevin joseph.py
+++ kevin joseph.py
@@ -1,14 +1,41 @@
 '''
-python program to find sum of digits of number
+python program to find the largest of three numbers
 author:kevin joseph
 '''
-num=int(input("enter a number:"))
-sum=0
-while(num>0):
-     r=num%10
-     num=num//10
-     sum=sum+r
-print(sum)
+
+num_1=int(input("enter the first number;"))
+num_2=int(input("enter the  second number;"))
+num_3=int(input("enter the  third number;"))
+
+if num_1>num_2 and num_1>num_3:
+     print("the largest number is :",num_1)
+elif num_2>num_1 and num_2>num_3:
+     print("the largest number is:",num_2)
+else:
+     print("the largest number is:",num_3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
